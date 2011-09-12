32a6dd0485224beaaf9f2bd0cf7245893f259ba1
Eric Kidd <git@randomhacks.net>
2011-09-12 12:38:52 -0400

    Validate fields in Post

--- app/models/rails_blog_engine/post.rb
+++ app/models/rails_blog_engine/post.rb
@@ -1,4 +1,6 @@
 module RailsBlogEngine
   class Post < ActiveRecord::Base
+    validates :title, :presence => true
+    validates :body, :presence => true
   end
 end
--- spec/models/rails_blog_engine/post_spec.rb
+++ spec/models/rails_blog_engine/post_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe RailsBlogEngine::Post do
+  it { should allow_value("Title").for(:title) }
+  it { should_not allow_value("").for(:title) }
+
+  it { should allow_value("Body").for(:body) }
+  it { should_not allow_value("").for(:body) }
 end
