42003aeced4f351655064a72a926fab12f194d87
Eric Kidd <git@randomhacks.net>
2011-09-12 14:14:10 -0400

    Require jquery-rails

--- lib/rails_blog_engine.rb
+++ lib/rails_blog_engine.rb
@@ -1,4 +1,8 @@
+# Manually require gems, because Bundler won't auto-require them when we're
+# running inside a gem.
+require "jquery-rails"
 require "haml"
+
 require "rails_blog_engine/engine"
 
 module RailsBlogEngine
