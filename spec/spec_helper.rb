40c11c078c45acbff882910f340d4e711d3f6fbd
Eric Kidd <git@randomhacks.net>
2011-09-12 12:03:02 -0400

    Fix include path for spec/support
    Again, see http://rubyx.com/2011/03/01/start-your-engines for details.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,12 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 
+# The root directory of our engine, and not our dummy application.
+ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each {|f| require f }
 
 RSpec.configure do |config|
   # == Mock Framework
