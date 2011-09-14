018e7f28e6263dd2fdec5beff674ff2b90f932f6
Eric Kidd <git@randomhacks.net>
2011-09-14 16:53:10 -0400

    Report filter-related errors aggressively
    The filter API is exposed to lots of potential errors, both from content
    authors and from filter creators.  We want to give useful error messages
    when possible.

--- lib/rails_blog_engine/filter.rb
+++ lib/rails_blog_engine/filter.rb
@@ -1,5 +1,12 @@
 module RailsBlogEngine
+  # A modular text filter that is applied to blog text before markdown
+  # processing.
   class Filter
+    # Override this method to transform +text+, using any supplied
+    # +arguments+.
+    def process(text, arguments)
+      raise "Please override #{self.class.name}#process"
+    end
 
     class << self
       FILTERS = {}
@@ -9,18 +16,28 @@ module RailsBlogEngine
         FILTERS[name] = new
       end
 
-      # Look up a registered filter class.
+      # Look up a registered filter class.  Raises an error if the filter
+      # can't be found.
       def for(name)
-        FILTERS[name]
+        FILTERS[name] or raise "Text filter not installed: #{name}"
       end
 
       # Apply all registered filters to the specified text.
       def apply_all_to(text)
         text.gsub(/<(filter|macro|typo):([_a-zA-z0-9]+)([^>]*)\/>/) do
-          self.for($2.to_sym).process(nil, parse_arguments($3))
+          trap_filter_errors do
+            self.for($2.to_sym).process(nil, parse_arguments($3))
+          end
         end.gsub(/<(filter|macro|typo):([_a-zA-z0-9]+)([^>]*)>(.*?)<\/\1:\2>/) do
-          self.for($2.to_sym).process($4, parse_arguments($3))
+          trap_filter_errors do
+            self.for($2.to_sym).process($4, parse_arguments($3))
+          end
         end
+      end
+
+      # :nodoc: Trap a filter's errors and return an error message.
+      def trap_filter_errors
+        yield rescue "<p><strong>#{$!.to_s}</strong></p>"
       end
 
       # :nodoc: Parse a filter arugment string.
@@ -30,7 +47,7 @@ module RailsBlogEngine
         args = {}
         until unparsed == ''
           m = unparsed.match(pattern)
-          m or raise "Can't parse filter arguments: <#{arg_string}>"
+          m or raise "Can't parse filter arguments: {{#{arg_string}}}"
           args[m[1].to_sym] = m[3] || m[4]
           unparsed = m[5]
         end
--- spec/lib/rails_blog_engine/filter_spec.rb
+++ spec/lib/rails_blog_engine/filter_spec.rb
@@ -15,6 +15,14 @@ describe RailsBlogEngine::Filter do
     end
   end
 
+  describe "#process" do
+    it "raises an error if not overridden" do
+      lambda do
+        Filter.new.process("text", {})
+      end.should raise_error(/override/)
+    end
+  end
+
   describe ".for" do
     it "returns the filter registered for a name" do
       Filter.for(:hello).should be_kind_of(HelloFilter)
@@ -36,12 +44,22 @@ END_OF_OUTPUT
       it "applies registered filters to '#{tag}' tags with content" do
         Filter.apply_all_to(<<"END_OF_INPUT").should == <<END_OF_OUTPUT
 <#{tag}:hello>Judy</#{tag}:hello>
-<#{tag}:hello class='example' >Mike</#{tag}:hello>
+<#{tag}:hello class='example' extra="" >Mike</#{tag}:hello>
 END_OF_INPUT
 <p>Hello, Judy!</p>
 <p class="example">Hello, Mike!</p>
 END_OF_OUTPUT
       end
+    end
+
+    it "reports errors inline" do
+        Filter.apply_all_to(<<END_OF_INPUT).should == <<END_OF_OUTPUT
+<filter:invalid/>
+<filter:hello class= >Mike</filter:hello>
+END_OF_INPUT
+<p><strong>Text filter not installed: invalid</strong></p>
+<p><strong>Can't parse filter arguments: {{ class= }}</strong></p>
+END_OF_OUTPUT
     end
   end
 end
