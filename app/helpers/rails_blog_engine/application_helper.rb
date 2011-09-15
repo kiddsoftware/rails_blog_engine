0dceb28d80956d4efa011ff00f6ab570dd23c6f2
Eric Kidd <git@randomhacks.net>
2011-09-14 20:09:50 -0400

    Apply filters to blog content

--- app/helpers/rails_blog_engine/application_helper.rb
+++ app/helpers/rails_blog_engine/application_helper.rb
@@ -1,7 +1,8 @@
 module RailsBlogEngine
   module ApplicationHelper
     def markdown(md_text)
-      sanitize(RDiscount.new(md_text, :smart).to_html)
+      filtered = Filters.apply_all_to(md_text)
+      sanitize(RDiscount.new(filtered, :smart).to_html)
     end
   end
 end
--- spec/helpers/rails_blog_engine/application_helper_spec.rb
+++ spec/helpers/rails_blog_engine/application_helper_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe RailsBlogEngine::ApplicationHelper do
+  class SillyFilter < RailsBlogEngine::Filters::Base
+    register_filter :silly
+
+    def process(text, options)
+      "Whee"
+    end
+  end
+
   describe ".markdown" do
     it "converts markdown text to HTML" do
       helper.markdown("_foo_ bar").should match(/<em>foo<\/em> bar/)
@@ -8,6 +16,10 @@ describe RailsBlogEngine::ApplicationHelper do
 
     it "does not pass scripts" do
       helper.markdown("<script>foo</script>").should_not match(/foo/)
+    end
+
+    it "applies filters" do
+      helper.markdown("<filter:silly/>").should match(/Whee/)
     end
   end
 end
